2d8c095d1f3c7055c61fddaec74e47fe02d37509
havving <sparkle5406@gmail.com>
2021-04-21 23:45:59 +0900

    0421 1. ComponentDependencyResolver

--- NodeBuilder-core/src/main/java/com/havving/framework/components/ComponentDependencyResolver.java
+++ NodeBuilder-core/src/main/java/com/havving/framework/components/ComponentDependencyResolver.java
@@ -8,6 +8,7 @@ import lombok.ToString;
 import java.lang.reflect.Field;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Stream;
 
 /**
  * Component 내에 Bind Annotation이 선언된 필드를 스캔하고, 각 객체간의 D/I 정의를 생성
@@ -18,6 +19,13 @@ import java.util.Set;
 public class ComponentDependencyResolver {
     @Getter
     private Set<DependencyIdMaps> values;
+
+    public static String getBindName(Field field) {
+        if (isBindableField(field))
+            return !field.getDeclaredAnnotation(Bind.class).name().equals("") ? field.getDeclaredAnnotation(Bind.class).name() : field.getName();
+        else
+            return null;
+    }
 
 
     /**
@@ -34,22 +42,45 @@ public class ComponentDependencyResolver {
 
 
     /**
-     *
      * @param policyFactory D/I 정책 내장 객체
      * @return 계산이 완료된 D/I 정의
      */
     public Set<DependencyIdMaps> resolve(ComponentPolicyFactory policyFactory) {
-        Set<DependencyIdMaps> idMapsSet = new HashSet<>();
+        Set<DependencyIdMaps> idMapList = new HashSet<>();
 
         policyFactory.getIds().forEach(id -> {
             BindPolicy policy = policyFactory.getPolicy(id);
             PolicyDefine define = policyFactory.getDefine(policy, id);
 
-            // TODO
+            Set<String> singleBindingTargetIds = _resolve(define, BindPolicy.Singleton);
+            Set<String> instantBindingTargetIds = _resolve(define, BindPolicy.Singleton);
+
+            DependencyIdMaps idMaps = new DependencyIdMaps(id, define.getClz(), singleBindingTargetIds, instantBindingTargetIds);
+
+            idMapList.add(idMaps);
         });
-        this.values = idMapsSet;
+
+        this.values = idMapList;
 
         return this.values;
+    }
+
+
+    private Set<String> _resolve(PolicyDefine define, BindPolicy bindPolicy) {
+        Set<String> result = new HashSet<>();
+        Class<?> clz = define.getClz();
+
+        Stream.of(clz.getDeclaredFields())
+                .filter(ComponentDependencyResolver::isBindableField)
+                .filter(field ->
+                        BindPolicy.is(field.getType().getDeclaredAnnotation(Component.class).singleton()).equals(bindPolicy)
+                )
+                .forEach(field -> {
+                    String bindId = ComponentDependencyResolver.getBindName(field);
+                    result.add(bindId);
+                });
+
+        return result;
     }
 
 
