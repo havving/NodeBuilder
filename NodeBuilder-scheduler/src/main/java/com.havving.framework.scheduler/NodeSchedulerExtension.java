1de07425b2ef65146a76fb95ff1eab63ded2edcc
havving <sparkle5406@gmail.com>
2021-05-05 23:35:23 +0900

    0505 1. create scheduler app

---
+++ NodeBuilder-scheduler/src/main/java/com.havving.framework.scheduler/NodeSchedulerExtension.java
@@ -0,0 +1,26 @@
+package com.havving.framework.scheduler;
+
+import com.havving.framework.Extension;
+import com.havving.framework.exception.ContainerInitializeException;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * Scheduler 모듈 활성화 객체
+ *
+ * @author HAVVING
+ * @since 2021-05-05
+ */
+public class NodeSchedulerExtension implements Extension<SchedulerContainer> {
+
+    @Override
+    public SchedulerContainer activate() throws ContainerInitializeException {
+        return null;
+    }
+
+    @Override
+    public boolean initializable() {
+        return false;
+    }
+}
---
+++ NodeBuilder-scheduler/src/main/java/com.havving.framework.scheduler/SchedulerContainer.java
@@ -0,0 +1,46 @@
+package com.havving.framework.scheduler;
+
+import com.havving.framework.components.Container;
+import com.havving.framework.exception.ContainerInitializeException;
+import org.quartz.JobDetail;
+import org.quartz.Trigger;
+
+import java.util.Map;
+
+/**
+ * @author HAVVING
+ * @since 2021-05-05
+ */
+public class SchedulerContainer implements Container<Map<String, SchedulerContainer.JobAndTriggerSet>> {
+
+
+    @Override
+    public CoreExtensions getExtensionType() {
+        return null;
+    }
+
+    @Override
+    public Map<String, JobAndTriggerSet> getFactory() {
+        return null;
+    }
+
+    @Override
+    public Container initializeToScan(String scanPackage) throws ContainerInitializeException {
+        return null;
+    }
+
+    @Override
+    public boolean valid() {
+        return false;
+    }
+
+    static class JobAndTriggerSet {
+        JobDetail jobDetail;
+        Trigger trigger;
+
+        JobAndTriggerSet(JobDetail jobDetail, Trigger trigger) {
+            this.jobDetail = jobDetail;
+            this.trigger = trigger;
+        }
+    }
+}
